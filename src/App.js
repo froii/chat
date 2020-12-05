2551f6c7995cb4acc8f83c521141de4d15a3894c
Git Server Admin <admin@git-server>
2020-12-05 10:27:31 +0000

    Add initial repository

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Controls from './Controls/index.js';
+import './style.css';
+
+
+const App = () => (
+    <Controls/>
+);
+
+export default App;
---
+++ src/Controls/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.css';
+
+export default () => (
+  <form className="controls">
+    <input placeholder="Say something"/>
+    <button>Send</button>
+  </form>
+);
